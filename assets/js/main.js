db2bb71e78d1112f584fdbf6f4f5bc8c0bca9f9c
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-10-05 12:47:02 +0600

    Add Js for about me page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 let goUP = document.querySelector(".go-up");
 let goDown = document.querySelector(".go-down");
-let about = document.queryCommandIndeterm(".about");
-let aboutHidden = document.queryCommandIndeterm(".about-hidden");
+let about = document.querySelector(".about");
+let aboutHidden = document.querySelector(".about-hidden");
 
 goDown.addEventListener("click", function(){
     about.classList.add("d-none");
+    aboutHidden.classList.remove("d-none");
+});
+
+goUP.addEventListener("click", function(){
+    about.classList.remove("d-none");
+    aboutHidden.classList.add("d-none");
 });
