322674044ca8b71d9a8fe405251c4e3dd74b7e21
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-10-07 19:51:10 +0600

    Animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,14 @@ let about = document.querySelector(".about");
 let aboutHidden = document.querySelector(".about-hidden");
 
 goDown.addEventListener("click", function(){
+    let aboutHiddenContent = document.querySelector(".about-hidden-content");
+    let aboutLeftAnimation = document.querySelector(".about-left-animation");
+    let aboutRightAnimation = document.querySelector(".about-right-animation");
     about.classList.remove("active");
     aboutHidden.classList.add("active");
+    aboutHiddenContent.classList.add("animate__fadeInLeft");
+    aboutLeftAnimation.classList.add("animate__rotateInDownLeft");
+    aboutRightAnimation.classList.add("animate__rotateInDownRight");
 });
 
 goUP.addEventListener("click", function(){
