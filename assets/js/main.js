c96d70e739cf451330ff5a245e87bbb873438564
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-11-09 15:21:54 +0600

    Navsection responsived for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
+//====================NAV SECTION START====================//
+let navTogoller = document.querySelector(".nav-section-wrapper-toggler");
+let navBar = document.querySelector(".nav-section-wrapper-navbar");
+
+navTogoller.addEventListener("click", function(){
+    navBar.classList.toggle("active");
+});
+//====================NAV SECTION ENDED====================//
+
+//====================ABOUT SECTION START====================//
 let goUP = document.querySelector(".go-up");
 let goDown = document.querySelector(".go-down");
 let about = document.querySelector(".about");
 let aboutHidden = document.querySelector(".about-hidden");
-
 goDown.addEventListener("click", function(){
     let aboutHiddenContent = document.querySelector(".about-hidden-content");
     let aboutLeftAnimation = document.querySelector(".about-left-animation");
@@ -18,3 +27,4 @@ goUP.addEventListener("click", function(){
     about.classList.add("active");
     aboutHidden.classList.remove("active");
 });
+//====================ABOUT SECITON ENDED====================//
